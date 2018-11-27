422bde283fbc42079175ec389ef010282c1e3750
bby360 <45206925+bby360@users.noreply.github.com>
2018-11-27 22:17:58 +0100

    header

--- views/home.php
+++ views/home.php
@@ -10,19 +10,8 @@
 <img id="fond" src="photo.jpg">
 <header>
 
-    <ul>
-        <li><a href="index.php?action=see_home">Accueil</a></li>
-        <li><a href="">Votre profil</a></li>
-        <li><a href="index.php?action=rooms">Pièces</a></li>
-        <li><a href="">Urgence</a></li>
-        <li><a href="">Catalogue</a></li>
-        <li><a href="">Consommation</a></li>
-        <li><a href="">Forum</a></li>
-        <li><a href="">FAQ</a></li>
-    </ul>
-    <div class="deconnexion">
-        <a href="">Déconnexion</a></li>
-    </div>
+        <?php require Projet-APPG4B/views/header.php ?>
+      
 </header>
 
 <section>
