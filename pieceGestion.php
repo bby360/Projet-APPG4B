a2d84bb5c6be3f8996ac16f231360b58c7eb8fd4
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2018-11-27 18:09:55 +0100

    Add files via upload

---
+++ pieceGestion.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+$db = new PDO('mysql:host=localhost;dbname=testsite;charset=utf8', 'root', '');
+
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=testsite;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+function Update(){
+$db -> exec('UPDATE Room INNER JOIN House ON "Room.idHouse"="House.idHouse" 
+			SET mode=$_POST["mode"] WHERE "House.idClient"=$_SESSION["id"] AND "Room.nom"=$_GET[]') //GET le nom de la piece
+
+if($_POST['mode']=='automatique'){
+	$db -> exec('UPDATE Room INNER JOIN House ON "Room.idHouse"="House.idHouse" 
+					SET lumiereManu=$_POST["lumiere_manuel"], ouvertureVolets=$_POST["ouverture_volets"], 
+					fermetureVolets=$_POST["fermeture_volets"], temperature=$_POST["temperature"]
+					WHERE "House.idClient"=$_SESSION["id"] AND "Room.nom"=$_GET[]')
+}
+else{
+	$db -> exec('UPDATE Room INNER JOIN House ON "Room.idHouse"="House.idHouse" 
+					SET lumiereAuto=$_POST["lumiere_auto"], ouvertureVolets=$_POST["ouverture_volets"], 
+					fermetureVolets=$_POST["fermeture_volets"], temperature=$_POST["temperature"]
+					WHERE "House.idClient"=$_SESSION["id"] AND "Room.nom"=$_GET[]')
+}
+
+?>
+
