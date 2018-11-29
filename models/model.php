9e348415b231dedc5669fe3663bb406782aa2279
bby360 <45206925+bby360@users.noreply.github.com>
2018-11-29 15:01:41 +0100

    Update model.php

--- models/model.php
+++ models/model.php
@@ -9,18 +9,6 @@ function getRooms(){
 
 function connection()
 {
-    if (isset($_POST['pseudo']) && !empty($_POST["Nom d'utilisateur"])
-        && !empty($_POST['password'])) {
-
-        if ($_POST['pseudo'] == 'test' &&
-            $_POST['password'] == '1234') {
-            $_SESSION['valid'] = true;
-            $_SESSION['timeout'] = time();
-            $_SESSION['username'] = 'test';
-
-            echo 'Bon pseudo et mot de passe';
-        } else {
-            $msg = 'Mauvais pseudo ou mot de passe';
-        }
+   
     }
 }
