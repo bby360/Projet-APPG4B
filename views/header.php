a86c097245610407925a7cdfa802e04f17365868
bby360 <45206925+bby360@users.noreply.github.com>
2018-12-03 17:27:24 +0100

    header actualisé

--- views/header.php
+++ views/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head>
  <meta charset="utf-8" />
  <title>Page d'accueil</title>
+
  <style> 
  header {
 position: relative;
@@ -38,6 +38,27 @@ li a:hover {
 	background-color: #111;
 }
 
+nav li:hover .submenu{
+	display: inline-block;
+	position: absolute;
+	top:100%;
+	left: 0px;
+	padding: 0px;
+	z-index: 100000;
+}
+
+.submenu li {
+	border-bottom: 1px solid #CCC;
+}
+
+.submenu li a {
+	padding: 15px 30px;
+	width: 270px;
+}
+
+nav ul li:hover a{
+	padding: 15px 30px 20px 30px;
+}
 #logo{
 	width: 100px;
 	height: 10px;
@@ -53,21 +74,28 @@ li:first-child{
 	background-color: #303030;
 }
 
+.submenu{
+	display:none;
+}
+
 @media screen and (max-width: 600px){
 	ul.topnav li.right,
 	ul.topnav li {float: none;}
 }
 
  </style>
-</head>
 <body>
  <header>
   <nav>
    <ul>
 	<li><a href="#"><img src="logonou.png" id="logo"></a></li>
-	<li><a href="#">Accueil</a></li>
+	<li><a href="index.php?action=see_home">Accueil</a></li>
 	<li><a href="#">Votre profil</a></li>
-	<li><a href="#">Pièces</a></li>
+	<li><a href="index.php?action=rooms">Pièces</a>
+	<ul class="submenu">
+	<li> <a href="#"> Ajouter une pièce</li>
+	<li> <a href="#">Voir mes pièces</li>
+	</ul>
 	<li><a href="#">Urgence</a></li>
 	<li><a href="#">Catalogue</a></li>
 	<li><a href="#">Consommation</a></li>
