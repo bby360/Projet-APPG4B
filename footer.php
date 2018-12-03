1630109630b36710ed7c322ddcf2b1e191664118
HannaTrvl <45207545+HannaTrvl@users.noreply.github.com>
2018-12-03 15:44:33 +0100

    Add files via upload
    C'est le footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <ul>
+        <li><a href="mentionlegal" id="mention">Mentions légales</a></li>
+        <li><a href="apropos" id="Apropos">A Propos</a></li>
+        <li> Created by</li>
+    </ul>
+    <div id="triangle"></div>
+    <img id="image2" src="logonou.png">
+</footer> 
+
+
+<?php try {
+                     <img scr="logo.png" alt="Logo CréAmbition" > 
+            }   catch (Exception $e){
+                    echo $e -> getMessage();
+
+            }?>
