034103e11d475cee6e0e1c0d676a61c23e9d8d4f
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2018-12-04 17:16:13 +0100

    Update model.php

--- models/model.php
+++ models/model.php
@@ -70,4 +70,30 @@ function connexion()
         include('index.php?action=connexion');
      }
     }
+function Update(){
+$req = $db -> prepare('UPDATE rooms JOIN house ON rooms.idHouse=house.idHouse SET mode= :nvmode WHERE house.idClient="1" AND nom="salon"'); 
+	$req -> execute(array(
+		'nvmode'=>$_POST['mode']));
+	$req->closeCursor();
+	if($_POST['mode']=='automatique'){
+		$req = $db -> prepare('UPDATE rooms JOIN house ON rooms.idHouse=house.idHouse SET lumiereAuto=:nvlumiereAuto, ouvertureVolets=:nvouvertureVolets, fermetureVolets=:nvfermetureVolets, temperature=:nvtemperature, ventilateur=:nvventilateur WHERE house.idClient="1" AND nom="salon"');
+		$req -> execute(array(
+			'nvlumiereAuto'=>$_POST['lumiere_auto'],
+			'nvouvertureVolets'=>$_POST['ouverture_volets'],
+			'nvfermetureVolets'=>$_POST['fermeture_volets'],
+			'nvtemperature'=>$_POST['temperature'],			
+			));
+	}
+	else{
+		$req = $db -> prepare('UPDATE rooms JOIN house ON rooms.idHouse=house.idHouse
+						SET lumiereManu=:nvlumiereManu, voletsManu=:nvvoletsManu, temperature=:nvtemperature, ventilateur=:nvventilateur
+						WHERE house.idClient="1" AND nom="salon"');
+		$req -> execute(array(
+			'nvlumiereManu'=>$_POST['lumiere_manuel'],
+			'nvvoletsManu'=>$_POST['volets_manuel'],
+			'nvtemperature'=>$_POST['temperature'],
+			
+		));
+	}
+}
 ?>
