f3ad82c507bc52d222d8bb2575a587e7fbc028ef
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2018-12-04 17:03:41 +0100

    Add files via upload

---
+++ views/catalogue.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Catalogue</title>
+    <link rel="stylesheet" type="text/css" href="./designs/css/catalogue.css" />
+</head>
+<body>
+<header>
+    <?php require "header.php"?>
+</header>
+
+<section>
+    
+</section>
+</body>
+</html>
