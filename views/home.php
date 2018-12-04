44412ad07f81177c6e1c357265cd739c4b8de4ef
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2018-12-04 17:19:10 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -2,52 +2,56 @@
 
 <html>
 <head>
-    <meta charset="utf-8" />
-    <title>Page d'accueil</title>
-    <link rel="stylesheet" type="text/css" href="designs/css/home.css" />
+ <meta charset="utf-8" />
+ <title>Page d'accueil</title>
+ <link rel="stylesheet" type="text/css" href="page_accueil.css" />
 </head>
 <body>
-<img id="fond" src="photo.jpg">
-<header>
-<?php
-include 'header.php';
-?>
+  <img id="fond" src="photo.jpg">
+ <header>  
+   <?php include("header.php"); ?>
+
 </header>
 
 <section>
 
-    <div class="en-tete">
-        <div class="info">
-            <ul>
-                <li>Jean Dupond</li>
-                <li>3 Avenue de Genève</li>
-                <li> Annecy 74000</li>
-            </ul>
-        </div>
-        <div class="photo">
 
-        </div>
-    </div>
-    <div class="salon"><a href="piece.html">
-            <div class="text">
-                <p>SALON</p>
-            </div>
-        </a>
-    </div>
-    <div class="bureau"><a href="bureau.html">
-            <div class="text">
-                <p>BUREAU</p>
-            </div>
-        </a>
-    </div>
+<div class="piece"><a href="index.php?action=room&amp;piece=salon">
+  <div class="text">
+    <p>PIECES</p>            
+  </div>
+</a>
+</div>
+
+<div class="profil"><a href="index.php?action=profil">            
+  <div class="text">
+    <p>PROFIL</p>            
+  </div>
+</a>
+</div>
+
+<div class="catalogue"><a href="index.php?action=catalogue">            
+  <div class="text">
+    <p>CATALOGUE</p>            
+  </div>
+</a>
+</div>
+
+<div class="consommation"><a href="index.php?action=consommation">            
+  <div class="text">
+    <p>CONSOMMATION</p>            
+  </div>
+</a>
+</div>
+
 </section>
 
 <footer>
-    <ul>
-        <li><a href="">Mentions légales</a></li>
-        <li><a href="">A Propos</a></li>
-        <li><a href="">Created by</a></li>
-    </ul>
-</footer>
+  <ul>
+   <li><a href="">Mentions légales</a></li>
+   <li><a href="">A Propos</a></li>
+   <li><a href="">Created by</a></li>
+ </ul>
+</footer>  
 </body>
 </html>
