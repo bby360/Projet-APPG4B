fd83ff05fbe8cdd9f418f9d13709ae471e77de5c
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2018-12-04 18:12:14 +0100

    Update catalogue.php

--- views/catalogue.php
+++ views/catalogue.php
@@ -11,7 +11,26 @@
 </header>
 
 <section>
-    
+    <div class="luminosite"><a href="index.php?action=see_home"></a>
+        <p>
+        <h1> Capteurs de luminosité: </h1>
+        <a href="index.php?action=see_home"><img src="designs/pictures/light.jpg" id="light" /></a>
+        </p>
+    </div>
+
+    <div class="temperature">
+        <p>
+        <h1>Capteurs de température :</h1>
+        <a href="index.php?action=see_home"><img src="designs/pictures/temprature.jpg" id="temperature" /></a>
+        </p>
+    </div>
+
+    <div class="presence">
+        <p>
+        <h1> Capteurs de présence :</h1>
+        <a href="index.php?action=see_home"><img src="designs/pictures/presence.jpg" id="presence"/><</a>
+        </p>
+    </div>
 </section>
 </body>
 </html>
