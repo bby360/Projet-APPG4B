c9e44c2ad779a46965f6c6a5bb0a5f110ed75824
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2018-12-04 18:14:25 +0100

    Add files via upload

---
+++ views/catalogue.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Catalogue</title>
+    <link rel="stylesheet" type="text/css" href="./designs/css/catalogue.css" />
+</head>
+<body>
+<header>
+    <?php require "header.php"?>
+</header>
+
+<section>
+
+    <div class="luminosite"><a href="index.php?action=see_home"></a>
+        <p>
+        <h1> Capteurs de luminosité: </h1>
+        <a href="index.php?action=see_home"><img src="designs/pictures/light.jpg" id="light" /></a>
+        </p>
+    </div>
+
+    <div class="temperature">
+        <p>
+        <h1>Capteurs de température :</h1>
+        <a href="index.php?action=see_home"><img src="designs/pictures/temprature.jpg" id="temperature" /></a>
+        </p>
+    </div>
+
+    <div class="presence">
+        <p>
+        <h1> Capteurs de présence :</h1>
+        <a href="index.php?action=see_home"><img src="designs/pictures/presence.jpg" id="presence"/><</a>
+        </p>
+    </div>
+
+</section>
+</body>
+</html>
