fb3b0b3fa06a6c492889f7392c1ee98c13ca405c
bby360 <45206925+bby360@users.noreply.github.com>
2018-12-04 17:35:53 +0100

    add deconnexio

---
+++ views/deconnexion.php
@@ -0,0 +1,26 @@
+<?php
+ 
+    // On démarre la session
+    session_start();
+ 
+    if isset($_SESSION['email']) { // Si tu es connecté on te déconnecte et on te redirige vers une page.
+ 
+        // Supression des variables de session et de la session
+        $_SESSION = array();
+        session_destroy();
+ 
+        // Supression des cookies de connexion automatique
+        setcookie('email', '');
+        setcookie('password', '');
+         
+        header('Location: index.php?deconnexion');
+ 
+    }else{ // Dans le cas contraire on t'empêche d'accéder à cette page en te redirigeant vers la page que tu veux.
+ 
+        header('Location: home.php');
+ 
+    }
+ 
+         
+ 
+?>
