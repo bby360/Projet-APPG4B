f1b43a03845976b0000821eafb09eaf87e24aa10
bby360 <45206925+bby360@users.noreply.github.com>
2018-12-04 17:13:58 +0100

    Add files via upload

--- models/model.php
+++ models/model.php
@@ -3,8 +3,71 @@
 require "models/dbconnexion.php";
 
 function getRooms(){
-    $db = dbConnect();
-    $req = $db->query("SELECT * FROM room");
-    return $req;
-}
+	$db = dbConnect();
 
+}
+$notification="Vous avez oublier de remplir un champs";
+
+function inscription()
+{
+    if (isset($_POST['lastName']) and isset($_POST['firstName']) and isset($_POST['email']) 
+    and isset($_POST['password']) and isset($_POST['phone']) and isset($_POST['adress'])) {
+
+        if (!empty($_POST['lastName']) and !empty($_POST['firstName']) and 
+        !empty($_POST['email']) and !empty($_POST['password']) and !empty($_POST['phone']) and 
+        !empty($_POST['adress'])) {
+
+            $lastName=$_POST['lastName'];
+            $firstName=$_POST['firstName'];
+            $email=$_POST['email'];
+            $password=$_POST['password'];
+            $phone=$_POST['phone'];
+            $adress=$_POST['adress'];
+
+            mysql_select_db("Domisep");
+            $requete= "INSERT INTO Client VALUES('','$lastName','$firstName','$email','$password','$phone','$adress')";
+        }
+    }
+    else { 
+        $notification;
+        include('index.php?action=inscription');
+
+    }
+}
+function connexion()
+{
+    if ( !empty($_POST['email']) and isset($_POST['password']) )
+    {
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+        mysql_select_db('Domisep');
+        $requete=$db->prepare('SELECT * FROM Client WHERE email= ? and mdp=?');
+        $req ->execute(array($email,$password));
+
+        while ($ligne=$requete->fetch()) {
+
+            if ($ligne['email']==$email and $ligne['password']==$password){
+
+                session_start();
+
+                $_SESSION['email']=$ligne['email'];
+                $_SESSION['password']=$ligne['password'];
+
+                echo "Vous vous êtes bien conenctés";         
+            }
+
+        else{ 
+            echo "Vous avez fait une erreur lors de la saisie du email/password, recommencez";
+
+            include('index.php?action=connexion');
+        }
+        }
+        
+    }
+    else {
+        $notification;
+        include('index.php?action=connexion');
+     }
+    }
+?>
