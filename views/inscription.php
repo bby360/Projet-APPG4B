a3a30305d858b917b400b9090c0b77dafc0fe58a
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2018-12-07 16:16:11 +0100

    Update inscription.php

--- views/inscription.php
+++ views/inscription.php
@@ -1,41 +1,52 @@
+<?php session_start()?>
 <!DOCTYPE html>
 
 <html>
 <head>
     <meta charset="utf-8" />
     <title>Inscription</title>
-    <link rel="stylesheet" type="text/css" href="inscription.css" />
+    <link rel="stylesheet" type="text/css" href="./designs/css/inscription.css" />
 </head>
 <body>
 
 <header>
+    
+    <?php require "header.php"?>
     <h1>VOTRE MAISON A PORTEE DE MAIN</h1>
+    
 </header>
 
 <section>
-    <form method="POST" action="index.php?action=inscription">
+    
+    <form method="post" action="index.php?action=inscription">
 
         <table align="center">
+            
             <tr>
                 <td>
                     <label>Nom :</label>
                 </td>
+                
                 <td>
-                    <input type="text" name="lastName" id="lastName"autofocus>
+                    <input type="text" name="name" autofocus>
                 </td>
             </tr>
+            
             <tr>
                 <td>
-                    <label>Prenom : </label>
+                    <label>Prénom : </label>
                 </td>
+                
                 <td>
-                    <input type="text" name="firstName" id="firstName" required>
+                    <input type="text" name="first_name" required>
                 </td>
             </tr>
+            
             <tr>
                 <td>
                     <label>Mail : </label>
                 </td>
+                
                 <td>
                     <input type="email" name="email" placeholder="Ex: jean.dupont@gmail.com" id="email" required>
                 </td>
@@ -45,48 +56,41 @@
                 <td>
                     <label>Mot de passe : </label>
                 </td>
+                
                 <td>
-                    <input type="password" name="password" required>
+                    <input type="text" name="password" required>
                 </td>
             </tr>
+            
             <tr>
                 <td>
                     <label>Confirmation mot de passe : </label>
                 </td>
+                
                 <td>
-                    <input type="password" name="confirm_password" required>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label>Phone : </label>
-                </td>
-                <td>
-                    <input type="phone" name="phone" required>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label>Adress  </label>
-                </td>
-                <td>
-                    <input type="text" name="adress" required>
+                    <input type="text" name="confirm_password" required>
                 </td>
             </tr>
 
         </table>
+        
         <p>
-            <input type="reset" value="Retour">
-            <input type="submit" value="valider" name="inscription" class="form-button">
+            <input type="reset" value="Retour"></a>
+            <input type="submit" value="Suivant">
         </p>
-    </form>
-    <p><a href="index.php?action=connexion">Déjà inscrit ?</a></p>
-
-
+    
 </section>
+    
+</form>
+
 <footer>
-    <div id="triangle"></div>
-    <p>Copyright - Tous droits réservés<br/>
+    
+   <?php require "footer.php"?>
+    
 </footer>
+
+</form>
+
 </body>
+
 </html>
