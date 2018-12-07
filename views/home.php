b392b12d269df271ad5e4607011922e37df6d5c9
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2018-12-07 13:19:50 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@
  <link rel="stylesheet" type="text/css" href="page_accueil.css" />
 </head>
 <body>
-  <img id="fond" src="./design/pictures/photo.jpg">
+  <img id="fond" src="./designs/pictures/photo.jpg">
  <header>  
    <?php include("header.php"); ?>
 
