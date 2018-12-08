8748bfb1b59eed586c944432ac515e1f8d248b23
GabrielAstieres <45207534+GabrielAstieres@users.noreply.github.com>
2018-12-08 20:50:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php require 'controller' ?>
+<?php
+require "controllers/controller.php";
+if(isset($_GET["action"])){
+    $action = htmlspecialchars($_GET["action"]);
+    switch ($action) {
+      
+      
+      default:
+        welcome();
+        break;
+
+    }
+} else {
+    welcome();
+}
