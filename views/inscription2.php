261735a6c85606a56d77253014b10ba68d5c51ca
GabrielAstieres <45207534+GabrielAstieres@users.noreply.github.com>
2018-12-08 21:19:55 +0100

    Create inscription2.php

---
+++ views/inscription2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="designs/css/inscription2.css">
+    <title>Inscription</title>
+</head>
+
+<body>
+<header><h1>Inscription</h1></header>
+
+<section>
+    <form method="post" action="index.php?action=inscription2">
+        <p><label for="Adresse">Adresse</label> : <input type="text" name="Adresse" id="Adresse" required=""></textarea></p>
+
+
+
+        <p><label for="CP">Code postal</label> : <input type="text" name="CP" id="CP" required=""></p>
+
+
+        <p>Veuillez consulter notre catalogue afin de selectionner les produits correspondants à vos besoins
+            <a href="catalogue.php"><input type="button" name="catalogue" value="Catalogue"></p></a>
+
+        <p>Une fois votre choix effectué, merci de prendre rendez-vous avec l'un de nos installateurs. <br>Celui-ci se rendra à votre domicile pour procéder à l'installation de votre system G4B.</p>
+
+
+        <p><label for="RDV">Prendre rendez-vous avec un technicien</label>  <input type="date" name="RDV" id="RDV"></p>
+
+        <p class="NotaBene">NB: La durée de l'installation varie en fonction du nombre de capteurs sélectionnés</p>
+
+        <a href="inscription.php"><input type="button" value="Retour"></a>
+        <a href="home.php"><input type="submit" value="Valider">
+    </form>
+</section>
+
+</body>
+</html>
