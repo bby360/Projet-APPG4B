31c7a6e1c62711654764c4693b953e951152c4ab
GabrielAstieres <45207534+GabrielAstieres@users.noreply.github.com>
2018-12-08 20:41:42 +0100

    Update dbconnexion.php

--- models/dbconnexion.php
+++ models/dbconnexion.php
@@ -1,11 +1,1 @@
-<?php
 
-function dbConnect(){
-	try{
-		$db = new PDO('mysql:host=localhost;dbname=fakeprojet;charset=utf8','root','');
-
-	}
-	catch(Exception $e){
-		die('erreur : '.$e->getMessage());
-	}
-}
