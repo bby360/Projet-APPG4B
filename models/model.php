6094e33492f6c65f1652451b153c9f1927743ccd
bby360 <45206925+bby360@users.noreply.github.com>
2018-12-08 21:31:49 +0100

    ajout dinscription

--- models/model.php
+++ models/model.php
@@ -1,1 +1,22 @@
+<?php
 
+require "models/dbconnexion.php";
+
+function inscrire(): bool
+{
+    $pass_hache = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+    $db=dbConnect();
+    $req = $db->prepare("INSERT INTO client(lastName, firstName, email, phone, adress, mdp) 
+    VALUES(:lastName, :firstName, :email, :phone, :adress, :mdp)");
+ 
+    $req->execute([
+    'lastName'=> $_POST['lastName'],
+    'firstName' => $_POST['firstName'],
+    'email'=> $_POST['email'],
+    'phone'=> $_POST['phone'],
+    'adress'=> $_POST['adress'],
+    'mdp'=> $pass_hache
+    ]);
+ 
+    return true;
+}
