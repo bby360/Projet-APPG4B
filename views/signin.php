f03fe582be79d1bb35b6dfd0499adfd2da4a4614
bby360 <45206925+bby360@users.noreply.github.com>
2018-12-08 22:10:50 +0100

    ajout signin

---
+++ views/signin.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Page d'accueil</title>
+    <link rel="stylesheet" type="text/css" href="./designs/css/connexion.css" />
+</head>
+<body>
+<header>
+    <?php require "header.php"?>
+    <h1>VOTRE MAISON A PORTEE DE MAIN</h1>
+
+</header>
+<section>
+
+    <div class = "connexion">
+
+        <form method="POST" action="">
+
+            <table align="center">
+                <tr>
+                    <td>
+                        <label>Mail</label>
+                    </td>
+                    <td>
+                        <input type="email" name="email" placeholder="Ex: jean.dupont@gmail.com" id="email" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Mot de passe</label>
+                    </td>
+                    <td>
+                        <input type="password" name="mdp" id="mdp" required>
+                    </td>
+                </tr>
+
+            </table>
+            <input type="submit" value="valider" name="connexion">
+        </form>
+        <p>
+                <a href="index.php?action=inscription">sinscrire </a>
+                <input type="submit" value="Mot de passe oublié">
+            </p>
+    </div>
+
+    <div class="catalogue">
+        <a href="catalogue.html">Catalogue</a>
+    </div>
+
+</section>
+
+
+<footer>
+        <p> Mentions légales
+            <img src="designs/pictures/Creaweb.jpg" id="creambition">
+        </p>
+    <p>Created by</p>
+</footer>
+</body>
+</html>
+
