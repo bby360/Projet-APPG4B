6a8329003d8b463f18bd1ae2b7dcf033cf666920
bby360 <45206925+bby360@users.noreply.github.com>
2018-12-08 20:53:13 +0100

    Add files via upload

---
+++ views/pageNonConnecter.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+ <meta charset="utf-8" />
+ <title>Page d'accueil</title>
+ <link rel="stylesheet" type="text/css" href="./designs/css/pageNonConnecter.css" />
+</head>
+<body>
+<img id="fond" src="fondAccueil.jpg">
+<header>
+</header>
+<section>
+
+    <div id="connexion">
+        <a href="#">Se Connecter</a>
+    </div>
+
+    <div id="inscrire">
+        <a href="#">S'inscrire</a>
+    </div>
+
+    <img id="G4B" src="G4B.jpg">
+
+    <p>
+        Gérez votre maison <br/>
+        où que vous soyez
+    </p>
+
+    <div id="design">
+        <p>designed by</p>
+        <img id="creambition" src="creambition.png">
+    </div>
+
+
+</section>
+</body>
+</html>
