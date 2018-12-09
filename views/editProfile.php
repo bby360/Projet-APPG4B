f0bb104ce7d4b9b1a9d2b0a0feef2bab5e3f813f
HannaTrvl <45207545+HannaTrvl@users.noreply.github.com>
2018-12-09 12:54:39 +0100

    link ok

--- views/editProfile.php
+++ views/editProfile.php
@@ -1,7 +1,8 @@
+<?php include("design/editProfile.css");?>
 <?php
 session_start();
-?>
 
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,9 +14,19 @@ session_start();
     <body>
     	<header>
             <ul>
-<?php include 'header.php';
-?>
-        </header>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Votre profil</a></li>
+                <li><a href="">Pièces</a></li>
+                <li><a href="">Urgence</a></li>
+                <li><a href="">Catalogue</a></li>
+                <li><a href="">Consommation</a></li>
+                <li><a href="">Forum</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+
+             <div class="deconnexion">
+                 <a href="">Déconnexion</a></li>
+             </div>
 
             <h1> Editer votre profil</h1></header>
 
@@ -29,9 +40,9 @@ session_start();
                 <fieldset>
                 <legend><h2>Changer votre contact</h2></legend>
                 <p>
-                    <h3><label for="numportable">Portable</label> : <input type="tel" name="numportable" id="numportable" /></h3>
+                    <h3><label for="phone">Portable</label> : <input type="tel" name="phone" id="phone" /></h3>
                     <h3><label for="email">E-mail</label> : <input type="email" name="email" id="email" /></h3>
-                    <h3><label for="urgence">Contact d'urgence</label> : <input type="text" name="urgence" id="urgence" /></h3>
+                    <h3><label for="emergency">Contact d'urgence</label> : <input type="text" name="emergency" id="emergency" /></h3>
 
                     <h3><input type="submit" value="Enregistrer" /></h3>
                 </p>
@@ -57,17 +68,6 @@ session_start();
         </div>
 
         </section>
-
-        <div class=temporaire>
-            <h4> <a href="">Ajouter une profil temporaire</a></h4>
-        </div>
-    	<a href="https://openclassrooms.com">OpenClassrooms</a>
-
-    <p>
-    Voici une photo que j'ai prise lors de mes dernières vacances à la montagne :<br />
-    <img src="montagne.jpg" alt="Photo de montagne" title="C'est beau les Alpes quand même !" />
-    <img src="../design/logoca.JPG" id="logo" alt="Logo CA">
-	</p>
 
         <footer>
 
