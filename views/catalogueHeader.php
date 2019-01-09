bdc1aa4c16c46bfaf16025c4fd9fddec285f9960
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2019-01-09 09:01:01 +0100

    Add files via upload

---
+++ views/catalogueHeader.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Header</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="./designs/css/header.css" />
+</head>
+
+<body>
+<header>
+    <nav>
+
+        <div id="menu">
+            <ul>
+                <li><a href="index.php?action=welcome"><img src="./designs/pictures/G4B.jpg" id="logo"></a></li>
+                <li><a href="index.php?action=lightSensorsUnlog">Capteur de Luminosité</a></li>
+                <li><a href="index.php?action=presenceSensorsUnlog">Capteur de Présence</a></li>
+                <li><a href="index.php?action=temperatureSensorsUnlog">Capteur de Température</a></li>
+
+
+                <li><a href="index.php?action=welcome">Retour à la page d'accueil</a></li>
+            </ul>
+        </div>
+
+    </nav>
+</header>
+</body>
+
+</html>
