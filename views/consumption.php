e52e889069572d7e2fd1d78fc12de7635f8ef03a
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2019-01-09 09:04:36 +0100

    Update consumption.php

--- views/consumption.php
+++ views/consumption.php
@@ -11,90 +11,42 @@
 </header>
 
 <section>
-
-    <script>
-        function move1() {
-            var elem = document.getElementById("myBar1");
-            var width = 1;
-            var id = setInterval(frame, 10);
-            function frame() {
-                if (width >= 100) {
-                    clearInterval(id);
-                } else {
-                    width++;
-                    elem.style.width = width + '%';
-                }
-            }
-        }
-
-        function move2() {
-            var elem = document.getElementById("myBar2");
-            var width = 1;
-            var id = setInterval(frame, 10);
-            function frame() {
-                if (width >= 100) {
-                    clearInterval(id);
-                } else {
-                    width++;
-                    elem.style.width = width + '%';
-                }
-            }
-        }
-
-        function move3() {
-            var elem = document.getElementById("myBar3");
-            var width = 1;
-            var id = setInterval(frame, 10);
-            function frame() {
-                if (width >= 100) {
-                    clearInterval(id);
-                } else {
-                    width++;
-                    elem.style.width = width + '%';
-                }
-            }
-        }
-    </script>
-
-    <table>
-        <h1> Consommation des capteurs de présence</h1>
-        <?php foreach($capteursP as $capteur) { ?>
-            <ul>
-                <li><?= $capteur["CSumP"];?></li>
-            </ul>
-        <?php } ?>
-
-        <div id="myProgress">
-            <div id="myBar1"></div>
+    <div class="gallery">
+        <a>
+            <img src="designs/pictures/light.jpg">
+        </a>
+        <div class="desc"><h1> Consommation des capteurs de Luminosité </h1>
+            <h1><?php foreach($capteursL as $capteur) { ?>
+                    <?= $capteur["CSumL"];?>
+                <?php } ?> kWh
+            </h1>
         </div>
-        <button onclick="move1()">Click Me</button>
+    </div>
 
-        <h1> Consommation de capteurs de Température</h1>
-        <?php foreach($capteursT as $capteur) { ?>
-            <ul>
-                <li><?= $capteur["CSumT"];?></li>
-            </ul>
-        <?php } ?>
-
-        <div id="myProgress">
-            <div id="myBar2"></div>
+    <div class="gallery">
+        <a>
+            <img src="designs/pictures/presence.jpg">
+        </a>
+        <div class="desc"><h1> Consommation des capteurs de présence</h1>
+            <h1>
+                <?php foreach($capteursP as $capteur) { ?>
+                    <?= $capteur["CSumP"];?>
+                <?php } ?> kWh
+            </h1>
         </div>
-        <button onclick="move2()">Click Me</button>
+    </div>
 
-        <h1> Consommation des capteurs de Luminosité </h1>
-        <?php foreach($capteursL as $capteur) { ?>
-            <ul>
-                <li><?= $capteur["CSumL"];?></li>
-            </ul>
-        <?php } ?>
-
-        <div id="myProgress">
-            <div id="myBar3"></div>
-        </div>
-        <button onclick="move3()">Click Me</button>
-
-
-    </table>
+    <div class="gallery">
+        <a>
+            <img src="designs/pictures/temperature.jpg">
+        </a>
+        <div class="desc"><h1> Consommation des capteurs de Température</h1>
+            <h1>
+                <?php foreach($capteursT as $capteur) { ?>
+                    <?= $capteur["CSumT"];?>
+                <?php } ?> kWh
+            </h1> </div>
+    </div>
 
 </section>
 <footer>
@@ -102,3 +54,4 @@
 </footer>
 </body>
 </html>
+
