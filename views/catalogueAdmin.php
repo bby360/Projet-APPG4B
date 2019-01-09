6d79d172862053bc7c6a6ee79db5224d8ffdeec6
AnaisBo <45207394+AnaisBo@users.noreply.github.com>
2019-01-09 10:17:11 +0100

    Add files via upload

---
+++ views/catalogueAdmin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Catalogue Administrateur</title>
+    <link rel="stylesheet" type="text/css" href="designs/css/catalogue.css" />
+</head>
+<body>
+<header>
+    <?php require "headerAdmin.php"?>
+</header>
+
+<section>
+
+</section>
+<footer>
+    <?php require "footer.php"?>
+</footer>
+</body>
+</html>
