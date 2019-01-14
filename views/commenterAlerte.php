590e81cbe9b798bb3028dd16c836010ab12d27ae
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2019-01-14 16:48:12 +0100

    Add files via upload

---
+++ views/commenterAlerte.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>CommenterAlerte</title>
+    <link rel="stylesheet" type="text/css" href="./designs/css/commenterAlerte.css" />
+</head>
+<body>
+<header>
+    <?php include("header.php"); ?>
+</header>
+ <p> Vous souhaitez déclarer une alerte pour un capteur de type <?= $_GET['capteur'];?> </p>
+<form action="index.php?action=posterAlerte&capteur=<?= $_GET['capteur'];?>" method="post" name="alerte">
+    <p>
+        <label for="typeAlerte">Type d'alerte</label><br />
+        <SELECT name="typeAlerte" id="typeAlerte" size="1">
+            <OPTION values="Ne fonctionne pas">Ne fonctionne pas</option>
+            <OPTION values="fonctionne par intermittence">fonctionne par intermittence</option>
+            <OPTION values="met du temps à repondre">met du temps à repondre</option>
+            <OPTION values="je l'ai cassé">je l'ai cassé</option>
+        </SELECT>
+    </p>
+    <p>
+        <label for="commentaire">Commentaire</label><br />
+        <textarea name="commentaire" id="commentaire" rows="10" cols="50"></textarea>
+    </p>
+    <div id="valider">
+        <input type="submit"  value="Valider" />
+    </div>
+</form>
