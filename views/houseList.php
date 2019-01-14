2021f2f39b8a0947a404321b7afba82722d06fb6
edouardlesage <edouard.lesage@yahoo.fr>
2019-01-14 17:39:45 +0100

    houseList

--- views/houseList.php
+++ views/houseList.php
@@ -8,7 +8,7 @@ dbConnect();
 <head>
     <meta charset="utf-8" />
     <title>Choix de la maison</title>
-    <link rel="stylesheet" type="text/css" href="" />
+    <link rel="stylesheet" type="text/css" href="../designs/css/houseList.css" />
 </head>
 
 <body>
@@ -16,7 +16,9 @@ dbConnect();
     <?php include "header.php"; ?>
 </header>
 
-<h1> Choix de l'habitation </h1>
+<h1> Choix de l'habitation </h1><br><br>
+
+<p>L'habitation que vous gérez actuellement est située à l'adresse <?php echo $_SESSION['adress'];?>, <?php echo $_SESSION['postalcode']?></p> <br><br>
 
 
 
