11b241ff5085df419284ca6d140714b3776cf123
edouard-lesage <45196876+edouard-lesage@users.noreply.github.com>
2019-01-16 10:36:58 +0100

    update for houseList

--- index.php
+++ index.php
@@ -132,9 +132,16 @@ if(isset($_GET["action"])){
             posterAlerte();
             break;
 
-        case 'houseList':
-            showHouse();
+        case 'seeHouse':
+            seeHouse();
             break;
+
+        case 'setHouse':
+            sethouse();
+            break;
+      default:
+        notFound();
+        break;
 
       default:
         notFound();
