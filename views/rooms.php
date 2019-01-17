4b6cd4ad4150ec09731c11291968a3d4895bf40f
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2019-01-17 17:38:10 +0100

    Update rooms.php

--- views/rooms.php
+++ views/rooms.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="./designs/css/rooms.css" />
 </head>
 <body>
-<!--<img id="fond" src="./designs/pictures/photo.jpg">-->
+<img id="fond" src="./designs/pictures/photo.jpg">
 <header class="header">
     <?php include("header.php"); ?>
 </header>
