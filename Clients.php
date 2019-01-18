6f1943989a41d61e90ef425d3b8d5fa9b2f1b0a6
HannaTrvl <45207545+HannaTrvl@users.noreply.github.com>
2019-01-18 13:28:57 +0100

    Add files via upload

---
+++ Clients.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<div xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="../design/Clients.css" />
+        <title>Fichier Clients</title>
+
+        <script type='text/javascript'>
+
+        </script>
+
+    </head>
+
+    <body>
+
+        <h1 id="top">Liste des clients</h1>
+
+    <?php
+    $bdd = new PDO('mysql:host=localhost;dbname=domisep;charset=utf8', 'root', 'root');
+
+    if (!$bdd) {
+        die("Connection failed: " . !$bdd);
+    }
+
+    $sql = "SELECT idClient, lastName, firstName, email, phone, adress, postalcode, emergency FROM client";
+    $result = $bdd->query($sql);
+
+    if ($result->rowCount() > 0) {
+        echo "<table class='clients'>
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>E-mail</th>
+            <th>Numéro de téléphone</th>
+            <th>Adresse</th>
+            <th>Code Postal</th>
+            <th>Numéro d'urgence</th>
+            <th>Voir le profil</th>
+        </tr>";
+        // output data of each row
+        while($row = $result->fetch()) {
+            echo "
+               <tr>
+                <td>".$row["idClient"]."</td>
+                <td>".htmlspecialchars($row["lastName"])."</td>
+                <td>".htmlspecialchars($row["firstName"])."</td>
+                <td>".htmlspecialchars($row["email"])."</td>
+                <td>".htmlspecialchars($row["phone"])."</td>
+                <td>".htmlspecialchars($row["adress"])."</td>
+                <td>".htmlspecialchars($row["postalcode"])."</td>
+                <td>".htmlspecialchars($row["emergency"])."</td>
+                <td>
+                <form action='clientProfile.php' method='GET'>
+                    <input type='text' name='idProfil' value='".$row['idClient']."' style='visibility:hidden' class='buttonHidden';>
+                    <input type=\"submit\" name='VoirProfil'  value='Voir profil ".$row["idClient"]."' class='button' style=\"vertical-align:middle\">
+                </form>
+                </td>
+               </tr>";
+
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    ?>
+        <a href="#top">Go to top</a>
+    </body>
--- ProfileEdited.php
+++ ProfileEdited.php
@@ -1,57 +1,159 @@
 <?php
-session_start();?>
-/**
-* Created by PhpStorm.
-* User: HannaTaravel
-* Date: 2018-12-03
-* Time: 18:10
-*/
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=domisep;charset=utf8', 'root', 'root');
+
+$requser = $bdd->prepare("SELECT * FROM client WHERE lastName = ?");
+$requser->execute(array($_SESSION['lastName']));
+$user = $requser->fetch();
+
+
+$_SESSION['email']=$user['email'];
+$_SESSION['phone']=$user['phone'];
+$_SESSION['emergency']=$user['emergency'];
+
+
+
+if ( !empty($_POST['email']) and empty($_POST['phone']) and empty($_POST['emergency'])) {
+
+    $newemail = htmlspecialchars($_POST['email']);
+
+    $insertmail = $bdd->prepare('UPDATE client SET email = ? WHERE lastName = ? ');
+    $insertmail->execute(array(
+        $newemail,
+        $_SESSION['lastName']
+    ));
+
+    $_SESSION['email']=$newemail;
+
+}
+elseif (!empty($_POST['phone']) and empty($_POST['email']) and empty($_POST['emergency'])){
+
+    $newphone = htmlspecialchars($_POST['phone']);
+    $insertphone = $bdd->prepare('UPDATE client SET phone = ? WHERE lastName = ?');
+    $insertphone->execute(array(
+        $newphone,
+        $_SESSION['lastName'],
+    ));
+
+    $_SESSION['phone']=$newphone;
+
+}
+elseif (!empty($_POST['phone']) AND !empty($_POST['email']) and empty($_POST['emergency'])){
+
+    $newphone = htmlspecialchars($_POST['phone']);
+    $newemail = htmlspecialchars($_POST['email']);
+    $req = $bdd->prepare('UPDATE client SET email = ?, phone = ? WHERE lastName = ?');
+    $req->execute(array(
+        $newemail,
+        $newphone,
+        $_SESSION['lastName'],
+    ));
+
+    $_SESSION['email']=$newemail;
+    $_SESSION['phone']=$newphone;
+}
+elseif(!empty($_POST['emergency']) and empty($_POST['phone']) and empty($_POST['email'])){
+
+    $newemergency = htmlspecialchars($_POST['emergency']);
+    $insertphone = $bdd->prepare('UPDATE client SET emergency = ? WHERE lastName = ?');
+    $insertphone->execute(array(
+        $newemergency,
+        $_SESSION['lastName'],
+    ));
+
+    $_SESSION['emergency']=$newemergency;
+
+}
+elseif(!empty($_POST['emergency']) and !empty($_POST['phone']) and empty($_POST['email'])){
+
+    $newphone = htmlspecialchars($_POST['phone']);
+    $newemergency = htmlspecialchars($_POST['emergency']);
+    $insertphone = $bdd->prepare('UPDATE client SET emergency = ?, phone = ? WHERE lastName = ?');
+    $insertphone->execute(array(
+        $newemergency,
+        $newphone,
+        $_SESSION['lastName'],
+    ));
+
+    $_SESSION['emergency']=$newemergency;
+    $_SESSION['phone']=$newphone;
+
+}
+elseif(!empty($_POST['emergency']) and empty($_POST['phone']) and !empty($_POST['email'])){
+
+    $newemail = htmlspecialchars($_POST['email']);
+    $newemergency = htmlspecialchars($_POST['emergency']);
+    $insertphone = $bdd->prepare('UPDATE client SET emergency = ?, email = ? WHERE lastName = ?');
+    $insertphone->execute(array(
+        $newemergency,
+        $newemail,
+        $_SESSION['lastName'],
+    ));
+
+    $_SESSION['emergency']=$newemergency;
+    $_SESSION['email']=$newemail;
+
+}
+elseif(!empty($_POST['emergency']) and !empty($_POST['phone']) and !empty($_POST['email'])){
+
+    $newphone = htmlspecialchars($_POST['phone']);
+    $newemail = htmlspecialchars($_POST['email']);
+    $newemergency = htmlspecialchars($_POST['emergency']);
+    $insertphone = $bdd->prepare('UPDATE client SET emergency = ?, phone = ?, email = ? WHERE lastName = ?');
+    $insertphone->execute(array(
+        $newemergency,
+        $newphone,
+        $newemail,
+        $_SESSION['lastName'],
+    ));
+
+    $_SESSION['emergency']=$newemergency;
+    $_SESSION['email']=$newemail;
+    $_SESSION['phone']=$newphone;
+
+}
+else{
+    echo 'Veuillez remplir un ou plusieurs champs';
+}
+
+
+?>
 
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="../design/ProfileEdited.css" />
-    <title>Profil Edité</title>
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="../design/ProfileEdited.css" />
+        <title>Profil</title>
+    </head>
 
-<p>
+    <body>
 
-<div class="Contact">
-    <p> <h1>Votre profil a été modifié : </h1>
-    <h3>Nom : <?php /* récupérer le nom de l'utilisateur*/ echo $_SESSION['nom']?> </h3>
-    <h3>Prénom : <?php /* récupérer le prénom de l'utilisateur*/ echo $_SESSION['prenom'] ?> </h3>
-    <h3>Numéro de portable : <?php echo $_POST['numportable']; ?> </h3>
-    <h3>E-mail : <?php echo $_POST['email']; ?></h3>
-    <h3>Contact d'urgence : <?php echo $_POST['urgence']; ?></h3>
-    </p>
-</div>
+    <section>
 
-<div class="Photo">
-    <p> <h1>Votre photo de profil a été modifié : </h1> </p>
+        <p> <h1>Votre profil</h1>
 
-    <?php
-    // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
-    if (isset($_FILES['newpic']) AND $_FILES['newpic']['error'] == 0)
-    {
-        // Testons si le fichier n'est pas trop gros
-        if ($_FILES['newpic']['size'] <= 1000000)
-        {
-            // Testons si l'extension est autorisée
-            $infosfichier = pathinfo($_FILES['newpic']['name']);
-            $extension_upload = $infosfichier['extension'];
-            $extensions_autorisees = array('jpg', 'jpeg', 'png');
-            if (in_array($extension_upload, $extensions_autorisees))
-            {
-                // On peut valider le fichier et le stocker définitivement
-                move_uploaded_file($_FILES['newpic']['tmp_name'], 'uploads/' . basename($_FILES['newpic']['name']));
-                echo "Votre photo a été modifiée !";
+        <div id="soussection">
 
-            }
-        }
-    }
-    ?>
+            <div class="Contact">
 
-</div>
+                <fieldset>
+                    <legend><h2>Vos informations</h2></legend>
+                    <p>
+                    <h3>Nom : <?php /* récupérer le nom de l'utilisateur*/ echo $_SESSION['lastName'];?> </h3>
+                    <h3>Prénom : <?php /* récupérer le prénom de l'utilisateur*/ echo $_SESSION['firstName']; ?> </h3>
+                    <h3>Téléphone : <?php echo $_SESSION['phone']; ?> </h3>
+                    <h3>E-mail : <?php echo $_SESSION['email'];?> </h3>
+                    <h3>Contact d'urgence : <?php echo $_SESSION['emergency'];?> </h3>
+                    </p>
+                </fieldset>
 
-</body>
+            </div>
+
+            <div class=edit>
+                <h4> <a href="editProfile.php">Modifier votre profil</a></h4>
+            </div>
+
+        </div>
+    </section>
+
+    </body>
