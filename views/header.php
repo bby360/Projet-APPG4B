6afe6b2261a9f77bcd33485343313f808e128270
mathiasdheilly <45207457+mathiasdheilly@users.noreply.github.com>
2019-01-19 15:55:49 +0100

    Update header.php

--- views/header.php
+++ views/header.php
@@ -25,7 +25,7 @@
                 <li><a href="index.php?action=rooms&amp;piece=Choisir une pièce">Pièces</a>
                     <ul>
                         <li><a href="index.php?action=roomList">Voir mes pièces</a></li>
-                        <li><a href="index.php?action=addRoom">Ajouter une pièce</a></li>
+                        <li><a href="index.php?action=seeAddRoom">Ajouter une pièce</a></li>
                     </ul>
                 </li>
                 <li><a href="index.php?action=addHouse">Vos maisons</a>
