1a76fa5675684592f15b4ab32a7c949a98059155
edouard-lesage <45196876+edouard-lesage@users.noreply.github.com>
2019-01-23 15:10:23 +0100

    cru

---
+++ views/cgu.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Conditions générales d'utilisation</title>
+    <link rel="stylesheet" type="text/css"  href="../designs/css/cgu.css" />
+</head>
+
+<body>
+<h1>CONDITIONS GÉNÉRALES D'UTILISATION</h1>
+
+
+<h3>En vigueur au 23/01/2019</h3><br>
+
+Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».<br><br>
+Les présentes CGU sont accessibles sur le site à la rubrique «CGU».<br><br>
+Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu et compris les CGU et je les accepte ».<br><br>
+En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.<br><br>
+www.G4B.com se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU. <br><br><br>
+<h2>ARTICLE 1 : LES MENTIONS LÉGALES</h2>
+L'édition du site www.G4B.com est assurée par la Société SARL Creambition au capital de XXXXX euros, immatriculée au RCS de XXXXXX sous le numéro XXXXX, dont le siège social est situé au XX rue XXX, XXXXX XXXXXX<br><br>
+Numéro de téléphone XXXXXXXX<br><br>
+Adresse e-mail : XX@XX.com<br><br>
+Le Directeur de la publication est : XXX<br><br>
+L'hébergeur du site www.G4B.com est la société XXX, dont le siège social est situé au XXX, avec le numéro de téléphone : XXXX.<br><br><br>
+<h2>ARTICLE 2 : ACCÈS AU SITE</h2>
+Le site www.G4B.com permet à l'Utilisateur un accès gratuit aux services suivants :
+
+
+Le site internet propose les services suivants :<br><br>
+- Gestion de la domestique d'une habitation <br>- Interaction avec le vendeur (SAV, etc.)<br><br>
+Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.<br><br>
+L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.<br><br>
+Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.<br><br>
+Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.<br><br>
+Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de www.G4B.com. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.<br><br>
+L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.<br><br><br>
+<h2>ARTICLE 3 : COLLECTE DES DONNÉES</h2>
+Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro XXXXXXX.<br><br>
+En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :<br><br>
+· via son espace personnel ;<br><br><br>
+<h2>ARTICLE 4 : PROPRIÉTÉ INTELLECTUELLE</h2>
+
+Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son...) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.<br><br>
+L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.<br><br>
+Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.<br><br>
+Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.<br><br><br>
+    <h2>ARTICLE 5 : RESPONSABILITÉ</h2>
+Les sources des informations diffusées sur le site www.G4B.com sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.<br><br>
+Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site www.G4B.com ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.<br><br>
+L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.<br><br>
+Le site www.G4B.com ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.<br><br>
+La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.<br><br><br>
+    <h2>ARTICLE 6 : LIENS HYPERTEXTES</h2>
+
+
+Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site www.G4B.com. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.<br><br><br>
+    <h2>ARTICLE 7 : COOKIES</h2>
+L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.<br><br>
+Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site www.G4B.com. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.<br><br>
+L’information contenue dans les cookies est utilisée pour améliorer le site www.G4B.com. En naviguant sur le site, L’Utilisateur les accepte.<br><br>
+L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.<br><br><br>
+    <h2>ARTICLE 8 : PUBLICATION PAR L’UTILISATEUR</h2>
+Le site permet aux membres de publier les contenus suivants : Forum, FAQ.<br><br>
+Dans ses publications, le membre s’engage à respecter les règles de la Netiquette (règles de bonne conduite de l’internet) et les règles de droit en vigueur.<br><br>
+Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s’en justifier auprès du membre.<br><br>
+Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle. Mais en publiant une publication sur le site, il cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé, dans le monde entier, sur tout support (numérique ou physique), pour la durée de la propriété intellectuelle. Le Membre cède notamment le droit d'utiliser
+
+
+sa publication sur internet et sur les réseaux de téléphonie mobile.<br><br>
+La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa publication.<br><br>
+Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.<br><br>
+Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site, sans préavis.<br><br><br>
+    <h2>ARTICLE 9 : DROIT APPLICABLE ET JURIDICTION COMPÉTENTE</h2>
+La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.<br><br>
+Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.<br><br><br><br>
+Réalisé sur https://www.legalplace.fr
+
+
+</body>
