49f86e9e20a8d9a4ccce838f16676aeaaa55a7a5
Billy <billy.travail@gmail.com>
2019-06-18 19:16:56 +0200

    ajout affichage capteur

--- views/faq.php
+++ views/faq.php
@@ -5,7 +5,6 @@ session_start();
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="designs/css/faq.css" />
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="./designs/css/faq.js"></script>
 <title>FAQ</title>
 </head>
--- views/logs.php
+++ views/logs.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+<?php
+if(!isset($_SESSION)) {
+session_start();
+} ?>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./designs/css/logs.css" />
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="./designs/css/logs.js"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>log</title>
 </head>
-<body>
-    <div>
+
+<header>
+<?php include("header.php"); ?>
+</header>
+
+<body class="body">
+    <div id="affichage">
 	<?php 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL,"http://projets-tomcat.isep.fr:8080/appService/?ACTION=GETLOG&TEAM=004B");
@@ -17,33 +28,36 @@
 	curl_close($ch);
 
 	$data_tab = str_split($data,33);
-	echo "Données des capteurs :<br />";
+	?>
+	<div data-splitting>Les données des capteurs</div>
+	<?php
 	$size=count($data_tab);
 	for($i=$size-4 ;$i<$size-1;$i++){
-		$trame = $data_tab[$i];   // décodage avec des substring
-		$t = substr($trame,0,1);
-		$o = substr($trame,1,4);  // ...// décodage avec sscanf
-        list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-        if ($c=51) {
-            echo "A $hour'h$min votre capteur de température indiquait $v degrée(s):<br />";
+		$trame = $data_tab[$i];  // décodage avec sscanf
+		list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+		if ($c=='3') {
+            echo "À $hour h$min votre capteur de température indiquait " .round(hexdec($v)). " degrée(s):<br />";
 		}
-		else if($c==53 & $v==1) {
-			echo "A $hour'h$min votre capteur de luminosité indique qu'il fait clair:<br />";
+		else if($c=='5' & $v=='1') {
+			echo "À $hour h$min votre capteur de luminosité indique qu'il fait clair:<br />";
 		}
-		else if($c==53 & $v==0) {
-			echo "A $hour'h$min votre capteur de luminosité indique qu'il fait sombre:<br />"; 
+		else if($c=='5' & $v=='0') {
+			echo "À $hour h$min votre capteur de luminosité indique qu'il fait sombre:<br />"; 
 		}
-		else if($c==57 & $v==1) {
-			echo "A $hour'h$min votre capteur de presence  indique qu'il y a quelqu'un:<br />"; 
+		else if($c=='7' & $v=='1') {
+			echo "À $hour h$min votre capteur de presence  indique qu'il y a quelqu'un:<br />"; 
 		}
-		else if($c==57 & $v==1) {
-			echo "A $hour'h$min votre capteur de presence  indique qu'il n'y a personne:<br />"; 
+		else if($c=='7' & $v=='0') {
+			echo "À $hour h$min votre capteur de presence  indique qu'il n'y a personne:<br />"; 
 		}
-		else { echo"erreur";}
+		else { echo"Pas de valeur <br />";}
 		//echo("Trame ".($i+1)." : $c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br/>");
     }   
 ?>
 </div>
-    
+<footer>
+    <?php require "footer.php"?>
+</footer>
 </body>
+
 </html>
